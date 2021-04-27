ea5975fcf241b504f72214a5fa87c298247814bf
Bruno <53619830+sklorde@users.noreply.github.com>
2021-04-27 15:38:59 -0300

    :art: show info about player

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,5 +1,8 @@
+import 'package:fifa_stats/app/widgets/card_player/card_player_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'home_controller.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,7 +14,37 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        color: Color.fromRGBO(28, 39, 51, 1),
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
+          child: Observer(
+            builder: (_) {
+              return SmartRefresher(
+                controller: controller.refreshController,
+                enablePullDown: false,
+                enablePullUp: false,
+                child: ListView.separated(
+                  physics: BouncingScrollPhysics(),
+                  itemCount: controller.players.length,
+                  itemBuilder: (context, index) {
+                    final player = controller.players[index];
+
+                    return CardPlayerWidget(
+                      player,
+                    );
+                  },
+                  separatorBuilder: (context, index) {
+                    return Padding(padding: EdgeInsets.only(top: 15));
+                  },
+                ),
+              );
+            },
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/app/widgets/card_player/card_player_widget.dart
@@ -0,0 +1,85 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:fifa_stats/app/shared/models/PlayerStats.dart';
+import 'package:flutter/material.dart';
+import 'package:octo_image/octo_image.dart';
+
+class CardPlayerWidget extends StatelessWidget {
+  final PlayerStats playerStats;
+
+  const CardPlayerWidget(this.playerStats);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print('click');
+      },
+      child: Container(
+        height: 100,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(22),
+          color: Color.fromRGBO(41, 55, 76, 1).withOpacity(0.42),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Row(
+            children: [
+              Container(
+                height: 64,
+                width: 64,
+                child: OctoImage(
+                  image: CachedNetworkImageProvider(playerStats.photoUrl),
+                  progressIndicatorBuilder: (context, progress) =>
+                      CircularProgressIndicator(
+                    backgroundColor: Color.fromRGBO(28, 39, 51, 0.9),
+                  ),
+                  errorBuilder: (context, error, stacktrace) =>
+                      Icon(Icons.error),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      playerStats.shortName,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        letterSpacing: 0.3,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    Text(
+                      playerStats.positions
+                          .cast()
+                          .toString()
+                          .replaceAll('[', '')
+                          .replaceAll(']', ''),
+                      style: TextStyle(
+                        color: Color.fromRGBO(143, 148, 155, 1),
+                        fontSize: 15,
+                        letterSpacing: 0.3,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Spacer(),
+              Text(
+                playerStats.overallRating.toString(),
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  letterSpacing: 0.3,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
