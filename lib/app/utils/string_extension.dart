814f0ea2da3949aa074d20f54b90600473dd087b
Bruno <53619830+sklorde@users.noreply.github.com>
2021-04-29 15:05:49 -0300

    :rocket: translate positions to pt-br

--- lib/app/utils/string_extension.dart
+++ lib/app/utils/string_extension.dart
@@ -2,4 +2,80 @@ extension StringExtension on String {
   toPosition() {
     return this.replaceAll('[', '').replaceAll(']', '');
   }
+
+  toLanguage() {
+    switch (this) {
+      case "GK":
+        return 'GOL';
+        break;
+
+      case "RB":
+        return 'LD';
+        break;
+
+      case "CB":
+        return 'ZAG';
+        break;
+
+      case "LB":
+        return 'LE';
+        break;
+
+      case "CDM":
+        return 'VOL';
+        break;
+
+      case "RM":
+        return "MD";
+        break;
+
+      case "CM":
+        return "MC";
+        break;
+
+      case "LM":
+        return "ME";
+        break;
+
+      case "CAM":
+        return "MEI";
+        break;
+
+      case "RW":
+        return "PD";
+        break;
+
+      case "ST":
+        return "ATA";
+        break;
+
+      case "LW":
+        return "PE";
+        break;
+
+      case "RWB":
+        return "ADD";
+        break;
+
+      case "LWB":
+        return "ADE";
+        break;
+
+      case "RF":
+        return "MAD";
+        break;
+
+      case "CF":
+        return "SA";
+        break;
+
+      case "LF":
+        return "MAE";
+        break;
+
+      default:
+        return '';
+        break;
+    }
+  }
 }
