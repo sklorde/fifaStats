47e7bef195515c5a0070d1baa714f4d62f2685e6
Bruno <53619830+sklorde@users.noreply.github.com>
2021-04-28 21:38:42 -0300

    :art: change primary/secundary color

--- lib/app/utils/fifaStatsColors.dart
+++ lib/app/utils/fifaStatsColors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class FifaStatsColors {
-  static const primary = Color.fromRGBO(23, 21, 20, 1);
-  static const secundary = Color.fromRGBO(35, 34, 34, 1);
+  static const primary = Color.fromRGBO(26, 29, 36, 1);
+  static const secundary = Color.fromRGBO(41, 48, 56, 1);
   static const tertiary = Color.fromRGBO(104, 98, 83, 1);
 }
