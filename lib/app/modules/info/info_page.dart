4aa05ed0f331746b9226d16c75784b9500542504
Bruno <53619830+sklorde@users.noreply.github.com>
2021-05-02 23:52:57 -0300

    :art: implements infopage

--- lib/app/modules/info/info_page.dart
+++ lib/app/modules/info/info_page.dart
@@ -1,6 +1,8 @@
 import 'package:fifa_stats/app/shared/models/PlayerStats.dart';
+import 'package:fifa_stats/app/utils/fifaStatsColors.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_radar_chart/flutter_radar_chart.dart';
 
 import 'info_controller.dart';
 
@@ -21,11 +23,54 @@ class InfoPageState extends State<InfoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: FifaStatsColors.primary,
       appBar: AppBar(
+        elevation: 0,
+        backgroundColor: FifaStatsColors.primary,
         title: Text(widget.playerStats.shortName),
       ),
-      body: Column(
-        children: <Widget>[],
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(15, 15, 15, 15),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Spacer(),
+                CircleAvatar(
+                  backgroundImage: NetworkImage(
+                    widget.playerStats.photoUrl,
+                  ),
+                  radius: 100,
+                  backgroundColor: FifaStatsColors.secundary,
+                ),
+                Spacer(),
+                Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 25,
+                      backgroundColor: FifaStatsColors.secundary,
+                      child: Image.network(
+                        widget.playerStats.logos.country.url,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    CircleAvatar(
+                      radius: 25,
+                      backgroundColor: FifaStatsColors.secundary,
+                      child: Image.network(
+                        widget.playerStats.logos.club.url,
+                        height: 40,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
